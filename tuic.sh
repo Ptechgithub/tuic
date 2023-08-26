7c8b9e150f38fbcb8f421c77428e296249c5ab58
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-08-27 02:48:48 +0330

    update tuic.sh

--- tuic.sh
+++ tuic.sh
@@ -280,7 +280,7 @@ LimitNOFILE=infinity
 [Install]
 WantedBy=multi-user.target
 EOF
-    url="tuic://$uuid:$passwd@$domain:$oldport/?congestion_control=bbr&udp_relay_mode=quic&alpn=h3%2Cspdy%2F3.1&allow_insecure=1#Peyman-Tuic"
+    url="tuic://$uuid:$passwd@$domain:$port/?congestion_control=bbr&udp_relay_mode=quic&alpn=h3%2Cspdy%2F3.1&allow_insecure=1#Peyman-Tuic"
     echo $url > /root/tuic/tuic.txt
     
     systemctl daemon-reload
